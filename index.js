837e62410513270da1d34ac2414e715ca141a53f
davisgd <davis.diede@gmail.com>
2017-06-29 10:50:44 -0600

    Added post route.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/instagrams",function(req,res){
   res.send("Hi!");
 });
 
+app.post("/instagrams",function(req,res){
+  res.send("Hello from post!");
+});
+
 var server = app.listen(3000,function(){
   console.log("Listening on port 3000");
 });

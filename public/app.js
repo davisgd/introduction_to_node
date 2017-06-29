4f0eb37bb7ae98295f832f06aa6a8221566b30e5
davisgd <davis.diede@gmail.com>
2017-06-29 13:20:13 -0600

    Finished app to insert pictures.

--- public/app.js
+++ public/app.js
@@ -1,4 +1,40 @@
 var apiURL = "/instagrams";
-var 
 
-var component = new Vue({ el: "#app", data: {}, methods: {} });
+var component = new Vue({
+  el: "#app",
+  data: {
+    instagrams: null,
+    newTitle: null,
+    newImage: null,
+  },
+  created: function(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData: function(){
+      var self = this;
+      $.ajax({
+        url: apiURL,
+        method: "GET",
+      }).done(function(response){
+        self.instagrams = response.data;
+        console.log(self.instagrams);
+      })
+    },
+    submitInstagram: function(){
+      var self = this;
+      var item = {
+        title: this.newTitle,
+        img: this.newImage,
+      }
+      $.ajax({
+        url: apiURL,
+        method: "POST",
+        data: item
+      }).done(function(response){
+        self.fetchData();
+        console.log(response);
+      });
+    }
+  }
+});
